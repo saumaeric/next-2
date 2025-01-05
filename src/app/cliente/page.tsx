d32cee9111a0b9ecd26992fb277a58e27f6298b8
Eric Sáuma <saumaeric@gmail.com>
2025-01-05 19:13:49 -0400

    Atualizei o readme,criei uma page de produto, criei uma page de cliente

---
+++ src/app/cliente/page.tsx
@@ -0,0 +1,7 @@
+export default function Cliente(){
+    return(
+        <div className="flex items-center justify-center w-screen bg-black text-white h-screen">
+            <h1 className="text-3xl">Cliente</h1>
+        </div>
+    )
+}
---
+++ src/app/produto/page.tsx
@@ -0,0 +1,7 @@
+export default function Produto(){
+    return(
+        <div>
+            <h1>Produto</h1>
+        </div>
+    )
+}
